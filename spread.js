f613d8030f2259df65af17c8453804462255035b
ravikrushnaassetmonk <ravikrushnaassetmonk@gmail.com>
2022-08-17 17:45:20 +0530

    Finished Session2

---
+++ spread.js
@@ -0,0 +1,2 @@
+let inputs = process.argv.slice(2);
+console.log(`The minimum of [${inputs}] is ${Math.min(...inputs)}`)
---
+++ templetString.js
@@ -0,0 +1,4 @@
+let inputs = (process.argv[2])
+//console.log(inputs)
+console.log((`Hello, ${inputs}!`))
+console.log(`Your name lowercased is "${inputs.toLowerCase()}".`)
